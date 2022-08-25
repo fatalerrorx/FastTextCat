54e8d70c8982133f2ea73ac1d7248a60bc7fb36d
fatalerrorx <222233+fatalerrorx@users.noreply.github.com>
2022-08-25 10:49:05 +1000

    Initial commit.

---
+++ FastTextCat.Test/CharacterNGramGeneratorTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FastTextCat.Test
+{
+    public class CharacterNGramGeneratorTest
+    {
+        private readonly string[] expectedNgrams = new[]
+        {
+            "t", "_t", "h", "th", "_th", "e", "he", "the", "_the", "e_", "he_", "the_", "_the_",
+            "q", "_q", "u", "qu", "_qu", "i", "ui", "qui", "_qui", "c", "ic", "uic", "quic", "_quic", "k", "ck", "ick", "uick", "quick", "k_", "ck_", "ick_", "uick_",
+            "b", "_b", "r", "br", "_br", "o", "ro", "bro", "_bro", "w", "ow", "row", "brow", "_brow", "n", "wn", "own", "rown", "brown", "n_", "wn_", "own_", "rown_",
+            "f", "_f", "o", "fo", "_fo", "x", "ox", "fox", "_fox", "x_", "ox_", "fox_", "_fox_"
+        };
+
+        [Test]
+        public void Test()
+        {
+            var ngrams = new HashSet<string>(new CharacterNGramGenerator(5).GetFeatures("The quick brown fox"));
+            Assert.True(ngrams.SetEquals(expectedNgrams));
+        }
+
+        [Test]
+        public void TestMaxLinesToRead()
+        {
+            var ngrams = new HashSet<string>(new CharacterNGramGenerator(1, 5).GetFeatures("abcdef\rghjjk\nlmn\nopq\r\nrstu\r\nvwxyz"));
+            
+            foreach (char ngram in "abcdefghjjklmnopqrstu")
+            {
+                Assert.True(ngrams.Contains(ngram.ToString()));
+            }
+
+            foreach (char ngram in "vwxyz")
+            {
+                Assert.False(ngrams.Contains(ngram.ToString()));
+            }
+        }
+    }
+}
---
+++ FastTextCat.Train/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FastTextCat.NaiveBayes;
+
+namespace FastTextCat.Test
+{
+    internal class Program
+    {
+        private static string DataDirectoryPath = Path.Combine(".", "Data");
+
+        private const string TrainingDataText = "x_train.txt";
+        private const string TrainginDataLanguage = "y_train.txt";
+
+        private const string TestingDataText = "x_test.txt";
+        private const string TestingDataLanguage = "y_test.txt";
+
+        static void Main(string[] args)
+        {
+            var identifier = train();
+            //var manager = new LanguageClassifierManager();
+            //var identifier = manager.LoadLanguageModelsAndCreateClassifier("WiLI-2018.xml");
+
+            testAccuracy(identifier);
+            
+            Console.ReadLine();
+        }
+
+        private static void testAccuracy(LanguageClassifier identifier)
+        {
+            var testingDataTextLines = File.ReadLines(Path.Combine(DataDirectoryPath, TestingDataText));
+            var testingDataLanguageLines = File.ReadLines(Path.Combine(DataDirectoryPath, TestingDataLanguage));
+            var stopWatch = new Stopwatch();
+
+            const int sampleInterval = 1000;
+
+            int total = 0;
+            int correct = 0;
+            foreach (var testingDataLine in testingDataLanguageLines.Zip(testingDataTextLines))
+            {
+                var result = identifier.Identify(testingDataLine.Second);
+                ClassificationResult<LanguageInfo> classificationResult = result.First();
+                if (classificationResult.Category.Iso639_2T == testingDataLine.First)
+                {
+                    correct++;
+                }
+                
+                total++;
+
+                if (total % sampleInterval == 0 && total > 0)
+                {
+                    Console.WriteLine($"Percent correct: {correct / (double)total * 100}");
+
+                    if (stopWatch.IsRunning)
+                    {
+                        stopWatch.Stop();
+                        Console.WriteLine($"Classifications per second: {1000 / (stopWatch.ElapsedMilliseconds / (double)sampleInterval)}");
+                    }
+
+                    stopWatch.Restart();
+                }
+            }
+
+            Console.WriteLine($"Percent correct: {correct / (double)total * 100}");
+        }
+
+        private static LanguageClassifier train()
+        {
+            var trainingDataTextLines = File.ReadLines(Path.Combine(DataDirectoryPath, TrainingDataText));
+            var trainingDataLanguageLines = File.ReadLines(Path.Combine(DataDirectoryPath, TrainginDataLanguage));
+            var trainingData = new Dictionary<string, StringBuilder>();
+
+            foreach (var traingingDataLine in trainingDataLanguageLines.Zip(trainingDataTextLines))
+            {
+                StringBuilder? stringBuilder;
+
+                if (!trainingData.TryGetValue(traingingDataLine.First, out stringBuilder))
+                {
+                    stringBuilder = new StringBuilder();
+                    trainingData.Add(traingingDataLine.First, stringBuilder);
+                }
+
+                stringBuilder.AppendLine(traingingDataLine.Second);
+            }
+
+            var manager = new LanguageClassifierManager();
+            var input = trainingData
+                .Select(f =>
+                {
+                    LanguageInfo languageInfo = new LanguageInfo(f.Key, "", "", "");
+                    TextReader textReader = new StringReader(f.Value.ToString());
+
+                    return Tuple.Create(languageInfo, textReader);
+                });
+
+            manager.TrainAndSaveLanguageModels(input, "WiLI-2018.xml");
+            return manager.LoadLanguageModelsAndCreateClassifier("WiLI-2018.xml");
+        }
+    }
+}
